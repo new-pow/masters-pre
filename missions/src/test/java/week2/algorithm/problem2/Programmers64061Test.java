cc29f91f9f6035a156c29d6ed3608d5f8522d4a4
saehim <saeilee.vxv@gmail.com>
2022-11-09 23:02:36 +0900

    Test: 문제6 테스트

--- missions/src/test/java/week2/algorithm/problem2/Programmers64061Test.java
+++ missions/src/test/java/week2/algorithm/problem2/Programmers64061Test.java
@@ -1,4 +1,12 @@
-import static org.junit.jupiter.api.Assertions.*;
+package week2.algorithm.problem2;
+
+import org.junit.jupiter.api.Test;
+
 class Programmers64061Test {
-  
+    @Test
+    void name() {
+        int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+        int[] moves = {1,5,3,5,1,2,1,4};
+        Programmers64061.solution(board,moves);
+    }
 }
