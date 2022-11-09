92098a13757446fe8a60fa993d080d8afa4d0644
saehim <saeilee.vxv@gmail.com>
2022-11-09 23:00:29 +0900

    Feat: 알고리즘 문제2 풀이

--- missions/src/main/java/week2/algorithm/problem2/Programmers64061.java
+++ missions/src/main/java/week2/algorithm/problem2/Programmers64061.java
@@ -1,2 +1,47 @@
-package week2.algorithm.problem2;public class Programmers64061 {
+package week2.algorithm.problem2;
+
+import java.util.*;
+
+public class Programmers64061 {
+    public static int solution(int[][] board, int[] moves) {
+        Stack<Integer> basket = new Stack<>();
+        int count = 0;
+
+        for (int move : moves) {
+            int doll = getDoll(board, move-1, 0);
+            if (doll != 0) {
+                count = putDoll(basket, doll, count);
+            }
+        }
+
+        return count;
+    }
+
+    public static int getDoll(int[][] board, int move, int deep) {
+        int search=0;
+        while (search==0) {
+            search = board[deep][move];
+            if (search!=0) {
+                board[deep][move] = 0;
+                break;
+            } else if (search==0 && deep==board.length-1) {
+                break;
+            } else if (search==0) {
+                deep ++;
+            }
+        }
+        return search;
+    }
+
+    public static int putDoll(Stack<Integer> basket, int doll, int count) {
+        if (basket.isEmpty()) {
+            basket.push(doll);
+        } else if (basket.peek()==doll) {
+            basket.pop();
+            count += 2;
+        } else if (basket.peek()!=doll) {
+            basket.add(doll);
+        }
+        return count;
+    }
 }
