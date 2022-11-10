6bb833c3464a0e9314b22025285258cb8ddbb6f9
saehim <saeilee.vxv@gmail.com>
2022-11-10 22:44:04 +0900

    Feat: Car 클래스

--- missions/src/main/java/week2/mission2/problem1/Car.java
+++ missions/src/main/java/week2/mission2/problem1/Car.java
@@ -1,4 +1,22 @@
 package week2.mission2.problem1;
 
-public class Problem1 {
+public class Car {
+    private int carNo;
+
+    public int getCarNo() {
+        return carNo;
+    }
+
+    public void setCarNo(int carNo) {
+        this.carNo = carNo;
+    }
+
+    public Car(int carNo) {
+        this.carNo = carNo;
+    }
+
+    public String showCarNumber() {
+        String carNo = String.valueOf(getCarNo());
+        return carNo;
+    }
 }
