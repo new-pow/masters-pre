ed13e7a48785fa3d8837d262484cf25736f7da64
saehim <saeilee.vxv@gmail.com>
2022-11-10 22:44:29 +0900

    Feat: Factory 추가

--- missions/src/main/java/week2/mission2/problem1/HyundaiFactory.java
+++ missions/src/main/java/week2/mission2/problem1/HyundaiFactory.java
@@ -1,2 +1,16 @@
-package week2.mission2.problem1;public class HyundaiFactory {
+package week2.mission2.problem1;
+
+public class HyundaiFactory {
+    // 인스턴스 만들때마다 누적되야 하므로 static
+    private static int id;
+
+    // 생성자 호출시 각 공장 인스턴스마다 초기화
+    public static HyundaiFactory getFactory() {
+        id = 1000;
+        return new HyundaiFactory();
+    }
+
+    public Car createCar() {
+        return new Car(id++);
+    }
 }
