5c90c87780dcb089f6015059cdc22f449a0ac4fd
saehim <saeilee.vxv@gmail.com>
2022-11-10 22:43:50 +0900

    Test: 문제1 제시된 테스트

--- missions/src/test/java/week2/mission2/problem1/CarTest.java
+++ missions/src/test/java/week2/mission2/problem1/CarTest.java
@@ -1,4 +1,14 @@
+package week2.mission2.problem1;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CarTest {
-  
+    public static void main(String[] args) {
+        HyundaiFactory factory = HyundaiFactory.getFactory();
+        Car myCar = factory.createCar();
+        Car yourCar = factory.createCar();
+
+        System.out.println("첫번째 차 번호는" +myCar.showCarNumber());
+        System.out.println("두번째 차 번호는" +yourCar.showCarNumber());
+    }
 }
