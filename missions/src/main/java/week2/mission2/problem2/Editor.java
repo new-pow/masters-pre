3ccd39733cba03b97957320a6dcd0cb062aa5671
saehim <saeilee.vxv@gmail.com>
2022-11-10 22:46:22 +0900

    Feat: 점수 수정하기

--- missions/src/main/java/week2/mission2/problem2/Editor.java
+++ missions/src/main/java/week2/mission2/problem2/Editor.java
@@ -1,4 +1,57 @@
 package week2.mission2.problem2;
 
-public class ScoreEditor {
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Editor {
+    School school;
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void askEdit(School school) throws IOException {
+        EditorPrinter.printEditorOn();
+        int input = sc.nextInt();
+        if (input == 1) {
+            runEditor(school);
+        }
+    }
+    public static void runEditor(School school) throws IOException {
+        System.out.println();
+        school.showAllStudentsId();
+
+        System.out.println();
+        EditorPrinter.printSelectStudent();
+        int studentId = sc.nextInt();
+        Student student = school.searchStudent(studentId);
+        Subject subject = selectScore(student);
+
+        editSubject(subject);
+        InputOutput.saveContents(school.getStudents());
+        school.showAllStudentsInfo();
+    }
+
+    private static void editSubject(Subject subject) {
+        try {
+            EditorPrinter.printEditScore();
+            int newScore = sc.nextInt();
+            subject.setScore(newScore);
+            EditorPrinter.printDone();
+        } catch (NumberFormatException e) {
+            EditorPrinter.printError();
+        }
+
+    }
+
+    private static Subject selectScore(Student student) {
+        int count = 1;
+        List<Subject> subjects = student.getSubjects();
+        for (Subject s : subjects) {
+            System.out.println(count++ + " " +s.getName() + " " + s.getScore());
+        }
+        EditorPrinter.printSelectSubject();
+        sc.nextLine();
+        int idx = sc.nextInt();
+
+        return subjects.get(idx-1);
+    }
 }
--- missions/src/main/java/week2/mission2/problem2/EditorPrinter.java
+++ missions/src/main/java/week2/mission2/problem2/EditorPrinter.java
@@ -1,10 +1,36 @@
 package week2.mission2.problem2;
 
-public class ScoreEditorPrinter {
-    private final String editorOn = "점수 정보 수정 1, 뒤로 2 >> ";
-    private final String selectStudent = "수정할 학생의 아이디를 입력하세요 >> ";
-    private final String selectSubject = "수정할 과목 아이디를 입력하세요 >> ";
-    private final String editSubject = "새로운 점수를 입력하세요 >> ";
-    private final String done = "수정 완료되었습니다.";
-    private final String error = "수정에 실패하였습니다.";
+public class EditorPrinter {
+    private static final String editorOn = "점수 정보 수정 1, 뒤로 2 >> ";
+    private static final String selectStudent = "수정할 학생의 아이디를 입력하세요 >> ";
+    private static final String selectSubject = "수정할 과목을 입력하세요 >> ";
+    private static final String editScore = "새로운 점수를 입력하세요 >> ";
+    private static final String done = "수정 완료되었습니다.";
+    private static final String error = "수정에 실패하였습니다.";
+    private static final String errorFindSubject = "해당 과목이 없습니다.";
+
+    public static void printEditorOn () {
+        System.out.print(editorOn);
+    }
+
+    public static void printSelectStudent () {
+        System.out.print(selectStudent);
+    }
+
+    public static void printSelectSubject () {
+        System.out.print(selectSubject);
+    }
+
+    public static void printEditScore () {
+        System.out.print(editScore);
+    }
+
+    public static void printDone () {
+        System.out.println(done);
+        System.out.println();
+    }
+
+    public static void printError () {
+        System.out.print(error);
+    }
 }
