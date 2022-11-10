e27c04efc701a2a055f9d05fe6d6028af0d0f98b
saehim <saeilee.vxv@gmail.com>
2022-11-11 01:26:57 +0900

    Test: 문제2 메소드 수정

--- missions/src/test/java/week2/mission2/problem2/StudentTest.java
+++ missions/src/test/java/week2/mission2/problem2/StudentTest.java
@@ -16,7 +16,7 @@ class StudentTest {
                 new Subject("국어", "100"),
                 new Subject("수학", "100")
         );
-        Student kim = school.getStudent("Kim","수학", kimScores);
+        Student kim = school.initStudent("Kim","수학", kimScores);
 
         // Lee
         List<Subject> leeScores = Arrays.asList(
@@ -24,7 +24,7 @@ class StudentTest {
                 new Subject("수학", "55"),
                 new Subject("영어", "100")
         );
-        Student lee = school.getStudent("Lee","국어", leeScores);
+        Student lee = school.initStudent("Lee","국어", leeScores);
 
         System.out.println(kim.getId());
         System.out.println(lee.getId());
