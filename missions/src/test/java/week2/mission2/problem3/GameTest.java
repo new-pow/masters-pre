da04c15c61a4f29276db1ca6c7ae8bd9d2c7e919
saehim <saeilee.vxv@gmail.com>
2022-11-11 01:26:37 +0900

    Test: 문제3 주사위, 과일 추가 테스트

--- missions/src/test/java/week2/mission2/problem3/GameTest.java
+++ missions/src/test/java/week2/mission2/problem3/GameTest.java
@@ -1,4 +1,25 @@
+package week2.mission2.problem3;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class GameTest {
-  
+    Game game;
+    @BeforeEach
+    void test() {
+         game = new Game();
+    }
+
+    @Test
+    void 주사위굴리기() {
+        System.out.println(game.rollDice());
+    }
+
+    @Test
+    void 과일삽입() {
+        int dice = 6;
+        game.getResult(0,6);
+    }
 }
