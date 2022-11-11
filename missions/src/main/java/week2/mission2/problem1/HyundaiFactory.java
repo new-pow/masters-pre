2c446700dab6e7b30dd6e27b213a9facad8a031e
saehim <saeilee.vxv@gmail.com>
2022-11-11 15:58:46 +0900

    refactor: 싱글톤으로 변경

--- missions/src/main/java/week2/mission2/problem1/HyundaiFactory.java
+++ missions/src/main/java/week2/mission2/problem1/HyundaiFactory.java
@@ -3,11 +3,18 @@ package week2.mission2.problem1;
 public class HyundaiFactory {
     // 인스턴스 만들때마다 누적되야 하므로 static
     private static int id;
+    private static HyundaiFactory hyundaiFactory;
+
+    public static HyundaiFactory getFactory() {
+        if (hyundaiFactory == null) {
+            hyundaiFactory = new HyundaiFactory();
+        }
+        return hyundaiFactory;
+    }
 
     // 생성자 호출시 각 공장 인스턴스마다 초기화
-    public static HyundaiFactory getFactory() {
+    private HyundaiFactory() {
         id = 1000;
-        return new HyundaiFactory();
     }
 
     public Car createCar() {
