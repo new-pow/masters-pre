eb8228bee7224560da94a3c5c4b97980b2494083
saehim <saeilee.vxv@gmail.com>
2022-11-11 16:00:38 +0900

    refactor: final 변수명 변경

--- missions/src/main/java/week2/mission2/problem2/EditorPrinter.java
+++ missions/src/main/java/week2/mission2/problem2/EditorPrinter.java
@@ -1,34 +1,34 @@
 package week2.mission2.problem2;
 
 public class EditorPrinter {
-    private static final String editorOn = "점수 정보 수정을 원한다면 1을 입력하세요 >> ";
-    private static final String selectStudent = "수정할 학생의 아이디를 입력하세요 >> ";
-    private static final String selectSubject = "수정할 과목코드를 입력하세요 >> ";
-    private static final String editScore = "새로운 점수를 입력하세요 >> ";
-    private static final String done = "수정 완료되었습니다.";
-    private static final String error = "수정에 실패하였습니다.";
+    private static final String EDITOR_ON = "점수 정보 수정을 원한다면 1을 입력하세요 >> ";
+    private static final String SELECT_STUDENT = "수정할 학생의 아이디를 입력하세요 >> ";
+    private static final String SELECT_SUBJECT = "수정할 과목코드를 입력하세요 >> ";
+    private static final String EDIT_SCORE = "새로운 점수를 입력하세요 >> ";
+    private static final String DONE = "수정 완료되었습니다.";
+    private static final String ERROR = "수정에 실패하였습니다.";
 
     public static void printEditorOn () {
-        System.out.print(editorOn);
+        System.out.print(EDITOR_ON);
     }
 
     public static void printSelectStudent () {
-        System.out.print(selectStudent);
+        System.out.print(SELECT_STUDENT);
     }
 
     public static void printSelectSubject () {
-        System.out.print(selectSubject);
+        System.out.print(SELECT_SUBJECT);
     }
 
     public static void printEditScore () {
-        System.out.print(editScore);
+        System.out.print(EDIT_SCORE);
     }
 
     public static void printDone () {
-        System.out.println(done+"\n");
+        System.out.println(DONE+"\n");
     }
 
     public static void printError () {
-        System.out.print(error+"\n");
+        System.out.print(ERROR+"\n");
     }
 }
