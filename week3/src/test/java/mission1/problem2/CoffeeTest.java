f558a51e7b9bcb8c57fbf1d4c7a564ee399e5b37
saehim <saeilee.vxv@gmail.com>
2022-11-15 13:38:53 +0900

    test(문제2) : 테스트 코드 입력

--- week3/src/test/java/mission1/problem2/CoffeeTest.java
+++ week3/src/test/java/mission1/problem2/CoffeeTest.java
@@ -1,4 +1,37 @@
+package mission1.problem2;
+
+import mission1.problem2.coffee.*;
+import mission1.problem2.topping.Mocha;
+import mission1.problem2.topping.WhippedCream;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CoffeeTest {
-  
+    @Test
+
+    public static void main(String[] args) {
+
+        Coffee etiopiaAmericano = new EtiopiaAmericano();
+        etiopiaAmericano.brewing();
+        System.out.println();
+
+        Coffee kenyaAmericano = new KenyaAmericano();
+        kenyaAmericano.brewing();
+        System.out.println();
+
+        Coffee kenyaLatte = new Latte(kenyaAmericano);
+        kenyaLatte.brewing();
+        System.out.println();
+
+        Coffee brazilMocha = new Mocha(new Latte(new BrazilAmericano()));
+        brazilMocha.brewing();
+        System.out.println();
+
+        Coffee hawaiiWhippedMocha =
+                new WhippedCream(new Mocha(new Latte( new HawaiiAmericano())));
+        hawaiiWhippedMocha.brewing();
+        System.out.println();
+
+    }
 }
