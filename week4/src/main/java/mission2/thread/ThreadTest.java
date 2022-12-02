b72525b59343aa71551b8b40b973a69100d8ac61
saehim <saeilee.vxv@gmail.com>
2022-12-02 13:49:07 +0900

    feat : thread test Thread 상속 받은 경우

--- week4/src/main/java/mission2/thread/ThreadTest.java
+++ week4/src/main/java/mission2/thread/ThreadTest.java
@@ -1,2 +1,26 @@
-package mission2.thread;public class ThreadTest {
+package mission2.thread;
+
+class MyThread extends Thread{
+
+    public void run() {
+
+        int i;
+        for(i = 0; i<200; i++) {
+            System.out.print(i + "\t");
+        }
+    }
+}
+
+public class ThreadTest {
+
+    public static void main(String[] args) {
+
+        System.out.println(Thread.currentThread());
+        MyThread th1 = new MyThread();
+        th1.start();
+
+        MyThread th2 = new MyThread();
+        th2.start();
+    }
+
 }
